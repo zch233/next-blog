154db062f51554bd6d0be610db209a9301ee24fd
zhangchao <529743595@qq.com>
2020-06-11 17:30:16 +0800

    Add Link

--- pages/post/post.jsx
+++ pages/post/post.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Post () {
   return (
-    <div>hello</div>
+    <div>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </div>
   )
 }
