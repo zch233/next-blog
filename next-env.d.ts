877f92a38c684f75e43831cf18e580c2137c269e
zhangchao <529743595@qq.com>
2020-06-18 21:49:47 +0800

    Add png,jpg Declear

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module "*.png" {
+  const value: string;
+  export default value
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value
+}
