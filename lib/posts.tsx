d4feb16b47dc8d641bb7d21ea5f882035f99c7b2
zhangchao <529743595@qq.com>
2020-06-21 21:39:30 +0800

    Add GetPost Api

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -2,10 +2,11 @@ import path from "path";
 import fs, {promises as fsPromise} from "fs";
 import matter from "gray-matter";
 
+const markdownDir = path.join(process.cwd(), '/markdown')
+
 export const getPosts = async () => {
-  const markdownDir = path.join(process.cwd(), '/markdown')
   const fileNames = await fsPromise.readdir(markdownDir)
-  const list:Post[] = fileNames.map(fileName => {
+  const list: Post[] = fileNames.map(fileName => {
     const filePath = path.join(markdownDir, fileName)
     const id = fileName.replace(/\.md$/, '')
     const text = fs.readFileSync(filePath, 'utf-8')
@@ -15,4 +16,13 @@ export const getPosts = async () => {
     }
   })
   return list
+}
+
+export const getPost = async (id: string) => {
+  const filePath = path.join(markdownDir, `${id}.md`)
+  const text = fs.readFileSync(filePath, 'utf-8')
+  const {data: {title, date}, content} = matter(text)
+  return {
+    id, title, date, content
+  }
 }
