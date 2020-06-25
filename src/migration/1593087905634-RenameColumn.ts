3b6d1ecd8367811d455b2d3d744e8b7b298dbafe
zhangchao <529743595@qq.com>
2020-06-25 20:27:50 +0800

    Clean Code Name

---
+++ src/migration/1593087905634-RenameColumn.ts
@@ -0,0 +1,17 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class RenameColumn1593087905634 implements MigrationInterface {
+
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.renameColumn('posts', 'author_id', 'authorId')
+    await queryRunner.renameColumn('comments', 'user_id', 'userId')
+    await queryRunner.renameColumn('comments', 'post_id', 'postId')
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.renameColumn('posts', 'authorId', 'author_id')
+    await queryRunner.renameColumn('comments', 'userId', 'user_id')
+    await queryRunner.renameColumn('comments', 'postId', 'post_id')
+  }
+
+}
