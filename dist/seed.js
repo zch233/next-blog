6679a1666d3d1cd321bb9ee03f65cc00ca0fece9
zhangchao <529743595@qq.com>
2020-06-25 18:00:33 +0800

    Add Seed

--- dist/seed.js
+++ dist/seed.js
@@ -10,18 +10,35 @@ require("reflect-metadata");
 
 var _typeorm = require("typeorm");
 
+var _Post = require("./entity/Post");
+
 (0, _typeorm.createConnection)().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
+    var posts;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            console.log("Inserting a new user into the database...");
-            console.log(connection);
-            _context.next = 4;
+            _context.next = 2;
+            return connection.manager.find(_Post.Post);
+
+          case 2:
+            posts = _context.sent;
+            console.log(posts);
+            _context.next = 6;
+            return connection.manager.save([1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(function (n) {
+              return new _Post.Post({
+                title: "\u7B2C".concat(n, "\u7BC7\u535A\u5BA2"),
+                content: "\u8FD9\u662F\u7B2C".concat(n, "\u7BC7\u535A\u5BA2\u7684\u5185\u5BB9")
+              });
+            }));
+
+          case 6:
+            console.log('数据填充了');
+            _context.next = 9;
             return connection.close();
 
-          case 4:
+          case 9:
           case "end":
             return _context.stop();
         }
