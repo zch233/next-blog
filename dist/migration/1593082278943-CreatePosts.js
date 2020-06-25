c801aa194c86af4a603ef11f22f752ef036cf529
zhangchao <529743595@qq.com>
2020-06-25 18:55:02 +0800

    Add Posts

---
+++ dist/migration/1593082278943-CreatePosts.js
@@ -0,0 +1,97 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.CreatePosts1593082278943 = void 0;
+
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
+
+var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
+
+var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
+
+var _typeorm = require("typeorm");
+
+var CreatePosts1593082278943 = /*#__PURE__*/function () {
+  function CreatePosts1593082278943() {
+    (0, _classCallCheck2["default"])(this, CreatePosts1593082278943);
+  }
+
+  (0, _createClass2["default"])(CreatePosts1593082278943, [{
+    key: "up",
+    value: function () {
+      var _up = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(queryRunner) {
+        return _regenerator["default"].wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                _context.next = 2;
+                return queryRunner.createTable(new _typeorm.Table({
+                  name: 'posts',
+                  columns: [{
+                    name: 'id',
+                    isGenerated: true,
+                    type: 'int',
+                    isPrimary: true,
+                    generationStrategy: "increment"
+                  }, {
+                    name: 'title',
+                    type: 'varchar'
+                  }, {
+                    name: 'content',
+                    type: 'text'
+                  }, {
+                    name: 'author_id',
+                    type: 'int'
+                  }]
+                }));
+
+              case 2:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee);
+      }));
+
+      function up(_x) {
+        return _up.apply(this, arguments);
+      }
+
+      return up;
+    }()
+  }, {
+    key: "down",
+    value: function () {
+      var _down = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee2(queryRunner) {
+        return _regenerator["default"].wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                _context2.next = 2;
+                return queryRunner.dropTable('posts');
+
+              case 2:
+              case "end":
+                return _context2.stop();
+            }
+          }
+        }, _callee2);
+      }));
+
+      function down(_x2) {
+        return _down.apply(this, arguments);
+      }
+
+      return down;
+    }()
+  }]);
+  return CreatePosts1593082278943;
+}();
+
+exports.CreatePosts1593082278943 = CreatePosts1593082278943;
---
+++ src/migration/1593082278943-CreatePosts.ts
@@ -0,0 +1,21 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreatePosts1593082278943 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'posts',
+            columns: [
+                {name: 'id', isGenerated: true, type: 'int', isPrimary: true, generationStrategy: "increment"},
+                {name: 'title', type: 'varchar'},
+                {name: 'content', type: 'text'},
+                {name: 'author_id', type:'int'}
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('posts')
+    }
+
+}
