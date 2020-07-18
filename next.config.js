c8c46e8e98bc94e482ef73e972d94391ce8f4667
zhangchao <529743595@qq.com>
2020-07-18 20:49:50 +0800

    Update file-loader

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   webpack: (config, options) => {
     config.module.rules.push({
-      test: /\.jpg$/,
+      test: /\.(jpg|png|gif|svg)$/,
       use: [
         {
           loader: 'file-loader',
           options: {
+            name: '[name].[hash].[ext]', // 文件名称
             outputPath: 'static', // 硬盘路径
             publicPath: '_next/static', // 网站路径
           }
