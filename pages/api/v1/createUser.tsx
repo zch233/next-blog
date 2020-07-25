96e1fa0cd682cda704c165d256b7e9414204ed22
zhangchao <529743595@qq.com>
2020-07-25 17:50:04 +0800

    Clean Code

--- pages/api/v1/createUser.tsx
+++ pages/api/v1/createUser.tsx
@@ -2,57 +2,20 @@ import {NextApiHandler} from 'next';
 import {User} from '../../../src/entity/User';
 import {getDatabaseConnection} from '../../../lib/getDatabaseConnection';
 
-interface Errors {
-  username: string[],
-  password: string[],
-  passwordConfirmation: string[],
-}
-
 const Posts: NextApiHandler = async (req, res) => {
   const {username = '', password = '', passwordConfirmation = ''} = req.body;
-  const errors: Errors = {
-    username: [],
-    password: [],
-    passwordConfirmation: [],
-  };
-  if (username.trim() === '') {
-    errors.username.push('用户名不能为空');
-  }
-  if (username.trim().length < 6) {
-    errors.username.push('用户名太短');
-  }
-  if (username.trim().length > 42) {
-    errors.username.push('用户名太长');
-  }
-  if (!/[A-Za-z0-9]/.test(username)) {
-    errors.username.push('用户名只能是字母和数字');
-  }
-  if (password === '') {
-    errors.password.push('密码不能为空');
-  }
-  if (password.length < 6) {
-    errors.password.push('密码太短');
-  }
-  if (passwordConfirmation === '') {
-    errors.passwordConfirmation.push('确认密码不能为空');
-  }
-  if (password !== passwordConfirmation) {
-    errors.passwordConfirmation.push('两次密码不一致');
-  }
   const connection = await getDatabaseConnection();
-  const hasUser = await connection.manager.find(User, {username});
-  if (hasUser.length > 0) {
-    errors.username.push('用户名已存在')
-  }
+  const user = new User();
+  user.username = username.trim();
+  user.password = password;
+  user.passwordConfirmation = passwordConfirmation
   res.setHeader('Content-Type', 'application/json');
-  if (Object.values(errors).find(v => v.length > 0)) {
+  await user.validate()
+  if (user.hasError()) {
     res.statusCode = 422;
-    res.write(JSON.stringify(errors));
+    res.write(JSON.stringify(user.errors));
   } else {
     res.statusCode = 200;
-    const user = new User();
-    user.username = username;
-    user.password = password;
     await connection.manager.save(user)
     res.write(JSON.stringify({mas: '成功'}));
   }
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,7 +1,12 @@
 import {Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
 import {Post} from "./Post";
 import {Comment} from './Comment'
-
+import {getDatabaseConnection} from '../../lib/getDatabaseConnection';
+interface Errors {
+  username: string[],
+  password: string[],
+  passwordConfirmation: string[],
+}
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn('increment')
@@ -18,4 +23,46 @@ export class User {
   posts: Post[];
   @OneToMany(type => Comment, comment => comment.user)
   comments: Comment[];
+
+  passwordConfirmation: string
+  errors: Errors = {
+    username: [],
+    password: [],
+    passwordConfirmation: [],
+  };
+  async validate () {
+
+    if (this.username === '') {
+      this.errors.username.push('用户名不能为空');
+    }
+    if (this.username.length < 6) {
+      this.errors.username.push('用户名太短');
+    }
+    if (this.username.length > 42) {
+      this.errors.username.push('用户名太长');
+    }
+    if (!/[A-Za-z0-9]/.test(this.username)) {
+      this.errors.username.push('用户名只能是字母和数字');
+    }
+    if (this.password === '') {
+      this.errors.password.push('密码不能为空');
+    }
+    if (this.password.length < 6) {
+      this.errors.password.push('密码太短');
+    }
+    if (this.passwordConfirmation === '') {
+      this.errors.passwordConfirmation.push('确认密码不能为空');
+    }
+    if (this.password !== this.passwordConfirmation) {
+      this.errors.passwordConfirmation.push('两次密码不一致');
+    }
+    const connection = await getDatabaseConnection();
+    const hasUser = await connection.manager.find(User, {username: this.username});
+    if (hasUser.length > 0) {
+      this.errors.username.push('用户名已存在')
+    }
+  };
+  hasError () {
+    return !!Object.values(this.errors).find(v => v.length > 0)
+  }
 }
