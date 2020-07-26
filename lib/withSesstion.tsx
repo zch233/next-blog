856f821b3838c73c26874b5032a91939b29d2b3f
zhangchao <529743595@qq.com>
2020-07-26 15:22:47 +0800

    Add .env.local to Save SessionPassword

--- lib/withSesstion.tsx
+++ lib/withSesstion.tsx
@@ -3,8 +3,7 @@ import {GetServerSideProps, NextApiHandler} from 'next';
 
 export function withSession(handler: NextApiHandler | GetServerSideProps) {
   return withIronSession(handler, {
-    // password: process.env.SECRET_COOKIE_PASSWORD,
-    password: 'c2a85490-cc60-4f21-94e8-8dc5dd3220da',
+    password: process.env.SECRET_COOKIE_PASSWORD,
     cookieName: 'blog',
     cookieOptions: {secure: false}
   });
