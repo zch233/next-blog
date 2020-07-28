0610f177b550e5789cefc39cf7f2fe9194ec94ec
zhangchao <529743595@qq.com>
2020-07-28 23:12:19 +0800

    Fix Page Key

--- hooks/usePager.tsx
+++ hooks/usePager.tsx
@@ -20,19 +20,17 @@ const Page = styled.a`
 const userPager = ({total, page, totalPage}: PagerOptions) => {
   const pageArr = []
   pageArr.push(1)
-  for (let i = page - 3; i <= page + 3; i++) {
+  for (let i = page - 2; i <= page + 2; i++) {
     pageArr.push(i);
   }
   pageArr.push(totalPage)
-  console.log(Array.from(new Set(pageArr))
-    .filter(number => number > 0 && number <= totalPage))
   const pager = (
     <div>
       {page > 1 && <Link href={`?page=${page-1}`}><a>上一页</a></Link>}
       {Array.from(new Set(pageArr))
         .filter(number => number > 0 && number <= totalPage)
         .reduce((result: number[], current) => current - (result[result.length - 1] || 0) === 1 ? result.concat(current): result.concat(-1, current), [])
-        .map(pageNumber => pageNumber === -1 ? <Page>...</Page> : <Link href={`?page=${pageNumber}`} key={pageNumber}><Page className={pageNumber === page && 'active'}>{pageNumber}</Page></Link>)}
+        .map((pageNumber, index) => pageNumber === -1 ? <Page key={index + '...'}>...</Page> : <Link href={`?page=${pageNumber}`} key={pageNumber}><Page className={pageNumber === page && 'active'}>{pageNumber}</Page></Link>)}
       {page < totalPage && <Link href={`?page=${page+1}`}><a>下一页</a></Link>}
       <span>共 {total} 条</span>
     </div>
