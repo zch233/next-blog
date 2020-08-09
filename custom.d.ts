2d3232eb51a6b583dd45da79415409356e1fede3
zhangchao <529743595@qq.com>
2020-08-09 19:51:26 +0800

    Fix Image Type

--- custom.d.ts
+++ custom.d.ts
@@ -5,3 +5,13 @@ type Post = {
   content?: string;
   htmlContent?: string;
 }
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
--- next-env.d.ts
+++ next-env.d.ts
@@ -1,26 +1,9 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-import * as next from 'next';
-
-declare module '*.png' {
-  const value: string;
-  export default value;
-}
-
 declare module 'next' {
   import {Session} from 'next-iron-session';
 
   interface NextApiRequest {
     session: Session
   }
-}
-
-declare module "*.png" {
-  const value: string;
-  export default value
-}
-
-declare module "*.jpg" {
-  const value: string;
-  export default value
 }
