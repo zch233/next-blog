aac421ffebb068eddfab10592e3499a29185919e
zhangchao <529743595@qq.com>
2020-08-17 22:30:55 +0800

    Add Post/index

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -18,6 +18,11 @@ interface Props {
 const Container = styled.div`
   max-width: 1200px;
   margin: 0 auto;
+  .title {
+    display: inline-block;
+    font-weight: 600;
+    font-style: normal;
+  }
   .author {
     cursor: pointer;
     color: rgba(0, 0, 0, 0.84);
@@ -26,6 +31,12 @@ const Container = styled.div`
     &:hover {
       text-decoration: underline;
     }
+  }
+  .content {
+    font-weight: 400;
+    color: rgba(0, 0, 0, 0.54);
+    font-size: 16px;
+    margin-bottom: 20px;
   }
   .time {
     color: rgba(0, 0, 0, 0.54);
@@ -100,10 +111,9 @@ const LeftSection = styled.section`
     overflow: hidden;
   }
   .title {
-    font-weight: 600;
     font-size: 24px;
     line-height: 28px;
-    margin: 17px 0 40px 0;
+    margin: 17px 0 13px 0;
   }
 `
 const RightSection = styled.section`
@@ -123,7 +133,6 @@ const RightSection = styled.section`
       display: inline-flex;
       flex-direction: column;
       .title {
-        font-weight: 600;
         font-size: 18px;
         line-height: 20px;
         margin-bottom: auto;
@@ -131,7 +140,62 @@ const RightSection = styled.section`
     }
   }
 `
-
+const PageMain = styled.main`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 70px;
+  margin-top: 70px;
+  border-top: 1px solid #ddd;
+`
+const ArticleList = styled.ul`
+  list-style: none;
+  flex: 1;
+  margin-right: 80px;
+  .articleItem {
+    display: flex;
+    &-info {
+      flex: 1;
+      .title {
+        font-size: 24px;
+        line-height: 28px;
+        margin-bottom: 12px;
+      }
+    }
+    .imageWrapper {
+      width: 150px;
+      margin-left: 25px;
+      height: 150px;
+      overflow: hidden;
+      background-color: #ddd;
+    }
+  }
+`
+const PopularList = styled.div`
+  width: 428px;
+  h4 {
+    font-weight: 600;
+    color: rgba(0,0,0,.84)
+    font-size: 24px;
+    padding-bottom: 20px;
+    border-bottom: 1px solid #ddd;
+  }
+  .popularItem {
+    display: flex;
+    padding-top: 36px;
+    em {
+      font-size: 34px;
+      color: rgba(0,0,0,.15);
+      font-style: normal;
+    }
+    &-info {
+      margin-left: 20px;
+      .title {
+        font-size: 18px;
+        margin-bottom: 10px;
+      }
+    }
+  }
+`
 const PostsIndex: NextPage<Props> = ({posts, ...pageOption}) => {
   const {pager} = userPager(pageOption);
   return (
@@ -164,7 +228,8 @@ const PostsIndex: NextPage<Props> = ({posts, ...pageOption}) => {
       <LatestPostsWrapper>
         <LeftSection>
           <div className={'imageWrapper'}></div>
-          <p className={'title'}>第一篇文章</p>
+          <cite className={'title'}>第一篇文章</cite>
+          <p className={'content'}>文章简介。。。。。。。。。。。。。。。。</p>
           <p className={'author'}>一只会飞的猪</p>
           <time className={'time'}>2019-10-12</time>
         </LeftSection>
@@ -172,13 +237,37 @@ const PostsIndex: NextPage<Props> = ({posts, ...pageOption}) => {
           <article className={'articleItem'}>
             <div className={'imageWrapper'}></div>
             <div className={'postInfo'}>
-              <p className={'title'}>第二篇文章</p>
+              <cite className={'title'}>第二篇文章</cite>
               <p className={'author'}>一只会飞的猪</p>
               <time className={'time'}>2019-10-12</time>
             </div>
           </article>
         </RightSection>
       </LatestPostsWrapper>
+      <PageMain>
+        <ArticleList>
+          <li className={'articleItem'}>
+            <div className={'articleItem-info'}>
+              <cite className={'title'}>这是文章的标题</cite>
+              <p className={'content'}>文章简介。。。。。。。。。。。。。。。。</p>
+              <p className={'author'}>作者</p>
+              <time className={'time'}>2020-01-05</time>
+            </div>
+            <div className={'imageWrapper'}></div>
+          </li>
+        </ArticleList>
+        <PopularList>
+          <h4>最近一周最热</h4>
+          <div className={'popularItem'}>
+            <em>01</em>
+            <div className={'popularItem-info'}>
+              <cite className={'title'}>这是文章的标题</cite>
+              <p className={'author'}>作者</p>
+              <time className={'time'}>2020-04-01</time>
+            </div>
+          </div>
+        </PopularList>
+      </PageMain>
       {posts.map(post => <p key={post.id}><Link href={'/posts/[id]'}
                                                 as={`/posts/${post.id}`}><a>{post.id}---{post.title}({post.authorId})</a></Link>
       </p>)}
