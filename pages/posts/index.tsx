9570d37887a43df8fbea6053731ddd6fd620d47f
zhangchao <529743595@qq.com>
2020-08-23 14:51:09 +0800

    Fix Styles

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -4,9 +4,19 @@ import React from 'react';
 import {Post} from '../../src/entity/Post';
 import Link from 'next/link';
 import {userPager} from '../../hooks/usePager';
-import headerImage from '../../assets/header.png'
-import logoImage from '../../assets/logo.png'
-import styled from 'styled-components';
+import headerImage from '../../assets/header.png';
+import logoImage from '../../assets/logo.png';
+import {
+  ArticleList,
+  Category,
+  CategoryWrapper, Container,
+  ImageWrapper, LatestPostsWrapper,
+  LeftSection,
+  PageHeader, PageMain,
+  PopularList,
+  RightSection, UserHeaderWrapper,
+  UserInfo,
+} from './indexStyles';
 
 interface Props {
   posts: Post[];
@@ -15,189 +25,12 @@ interface Props {
   totalPage: number;
   size: number;
 }
-const Container = styled.div`
-  max-width: 1200px;
-  margin: 0 auto;
-  .title {
-    display: inline-block;
-    font-weight: 600;
-    font-style: normal;
-  }
-  .author {
-    cursor: pointer;
-    color: rgba(0, 0, 0, 0.84);
-    font-size: 15px;
-    margin: 8px 0;
-    &:hover {
-      text-decoration: underline;
-    }
-  }
-  .content {
-    font-weight: 400;
-    color: rgba(0, 0, 0, 0.54);
-    font-size: 16px;
-    margin-bottom: 20px;
-  }
-  .time {
-    color: rgba(0, 0, 0, 0.54);
-    font-size: 15px;
-  }
-`
-const PageHeader = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 0;
-`
-const ImageWrapper = styled.div`
-  width: 150px;
-`
-const UserInfo = styled.div`
-  display: flex;
-  align-items: center;
-  > a {
-    background-color: rgb(212,80,80);
-    color: #fff;
-    padding: .6em 1.1em;
-    border-radius: 2em;
-    text-decoration: none;
-    transition: all .3s;
-    &:hover {
-      background-color: rgb(212,60,60);
-    }
-  }
-  > * {
-    margin: 0 20px;
-    &:last-child {
-      margin-right: 0;
-    }
-  }
-`
-const UserHeaderWrapper = styled.div`
-  border-radius: 50%;
-  width: 35px;
-  height: 35px;
-  overflow: hidden;
-  border: 1px solid #ddd;
-`
-const CategoryWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin: 10px 0 20px 0;
-`
-const Category = styled.ul`
-  list-style: none;
-  flex: 1;
-  display: inline-flex;
-  .item {
-    margin: 0 10px;
-    color: rgba(0,0,0,.54);
-    transition: all .3s;
-    &.active {
-      color: rgba(0,0,0,.84);
-    }
-  }
-`
-const LatestPostsWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-`
-const LeftSection = styled.section`
-  width: 50%;
-  border: 1px solid #ddd;
-  .imageWrapper {
-    background-color: #ddd;
-    height: 150px;
-    overflow: hidden;
-  }
-  .title {
-    font-size: 24px;
-    line-height: 28px;
-    margin: 17px 0 13px 0;
-  }
-`
-const RightSection = styled.section`
-  width: 49%;
-  border: 1px solid #ddd;
-  .articleItem {
-    display: flex;
-    .imageWrapper {
-      width: 100px;
-      height: 100px;
-      overflow: hidden;
-      margin-right: 20px;
-      background-color: #ddd;
-    }
-    .postInfo {
-      flex: 1;
-      display: inline-flex;
-      flex-direction: column;
-      .title {
-        font-size: 18px;
-        line-height: 20px;
-        margin-bottom: auto;
-      }
-    }
-  }
-`
-const PageMain = styled.main`
-  display: flex;
-  justify-content: space-between;
-  padding-top: 70px;
-  margin-top: 70px;
-  border-top: 1px solid #ddd;
-`
-const ArticleList = styled.ul`
-  list-style: none;
-  flex: 1;
-  margin-right: 80px;
-  .articleItem {
-    display: flex;
-    &-info {
-      flex: 1;
-      .title {
-        font-size: 24px;
-        line-height: 28px;
-        margin-bottom: 12px;
-      }
-    }
-    .imageWrapper {
-      width: 150px;
-      margin-left: 25px;
-      height: 150px;
-      overflow: hidden;
-      background-color: #ddd;
-    }
-  }
-`
-const PopularList = styled.div`
-  width: 428px;
-  h4 {
-    font-weight: 600;
-    color: rgba(0,0,0,.84)
-    font-size: 24px;
-    padding-bottom: 20px;
-    border-bottom: 1px solid #ddd;
-  }
-  .popularItem {
-    display: flex;
-    padding-top: 36px;
-    em {
-      font-size: 34px;
-      color: rgba(0,0,0,.15);
-      font-style: normal;
-    }
-    &-info {
-      margin-left: 20px;
-      .title {
-        font-size: 18px;
-        margin-bottom: 10px;
-      }
-    }
-  }
-`
+
 const PostsIndex: NextPage<Props> = ({posts, ...pageOption}) => {
   const {pager} = userPager(pageOption);
+  const [firstPost, ...otherPosts] = posts;
+  const subPosts = otherPosts.slice(0, 3);
+  const restPosts = otherPosts.slice(3)
   return (
     <Container>
       <PageHeader>
@@ -216,61 +49,83 @@ const PostsIndex: NextPage<Props> = ({posts, ...pageOption}) => {
       <CategoryWrapper>
         <div>&lt;</div>
         <Category>
-          <li className={'item'}>日记</li>
-          <li className={'item'}>日记</li>
-          <li className={'item active'}>日记</li>
-          <li className={'item'}>日记</li>
-          <li className={'item'}>日记</li>
-          <li className={'item'}>日记</li>
+          <li className={'item'}>分类一</li>
+          <li className={'item'}>分类二</li>
+          <li className={'item active'}>分类三</li>
+          <li className={'item'}>分类四</li>
+          <li className={'item'}>分类五</li>
+          <li className={'item'}>分类6</li>
         </Category>
         <div>&gt;</div>
       </CategoryWrapper>
       <LatestPostsWrapper>
         <LeftSection>
-          <div className={'imageWrapper'}></div>
-          <cite className={'title'}>第一篇文章</cite>
-          <p className={'content'}>文章简介。。。。。。。。。。。。。。。。</p>
+          <Link href={'/posts/[id]'} as={`/posts/${firstPost.id}`}>
+            <a>
+              <div className={'imageWrapper'}></div>
+              <cite className={'title'}>{firstPost.title}</cite>
+              <p className={'content'}>{firstPost.content.substr(0, 100)}...</p>
+            </a>
+          </Link>
           <p className={'author'}>一只会飞的猪</p>
-          <time className={'time'}>2019-10-12</time>
+          <time className={'time'}>{new Date(firstPost.createdAt).toDateString()}</time>
         </LeftSection>
         <RightSection>
-          <article className={'articleItem'}>
-            <div className={'imageWrapper'}></div>
-            <div className={'postInfo'}>
-              <cite className={'title'}>第二篇文章</cite>
-              <p className={'author'}>一只会飞的猪</p>
-              <time className={'time'}>2019-10-12</time>
-            </div>
-          </article>
+          {subPosts.map(post => (
+            <article key={post.id} className={'articleItem'}>
+              <Link href={'/posts/[id]'} as={`/posts/${post.id}`}>
+                <a><div className={'imageWrapper'}></div></a>
+              </Link>
+              <div className={'postInfo'}>
+                <Link href={'/posts/[id]'} as={`/posts/${post.id}`}>
+                  <a>
+                    <cite className={'title'}>{post.title}</cite>
+                    <p className={'content'}>{post.content}</p>
+                  </a>
+                </Link>
+                <p className={'author'}>一只会飞的猪</p>
+                <time className={'time'}>{new Date(post.createdAt).toDateString()}</time>
+              </div>
+            </article>
+          ))}
         </RightSection>
       </LatestPostsWrapper>
       <PageMain>
         <ArticleList>
-          <li className={'articleItem'}>
-            <div className={'articleItem-info'}>
-              <cite className={'title'}>这是文章的标题</cite>
-              <p className={'content'}>文章简介。。。。。。。。。。。。。。。。</p>
-              <p className={'author'}>作者</p>
-              <time className={'time'}>2020-01-05</time>
-            </div>
-            <div className={'imageWrapper'}></div>
-          </li>
+          {restPosts.map(post => (
+            <li className={'articleItem'}>
+              <div className={'articleItem-info'}>
+                <Link href={'/posts/[id]'} as={`/posts/${post.id}`}>
+                  <a>
+                    <cite className={'title'}>{post.title}</cite>
+                    <p className={'content'}>{post.content.substr(0, 150)}...</p>
+                  </a>
+                </Link>
+                <p className={'author'}>作者</p>
+                <time className={'time'}>{new Date(post.createdAt).toDateString()}</time>
+              </div>
+              <Link href={'/posts/[id]'} as={`/posts/${post.id}`}>
+                <a><div className={'imageWrapper'}></div></a>
+              </Link>
+            </li>
+          ))}
         </ArticleList>
         <PopularList>
           <h4>最近一周最热</h4>
-          <div className={'popularItem'}>
-            <em>01</em>
-            <div className={'popularItem-info'}>
-              <cite className={'title'}>这是文章的标题</cite>
-              <p className={'author'}>作者</p>
-              <time className={'time'}>2020-04-01</time>
+          {posts.slice(0, 5).map((post, index) => (
+            <div className={'popularItem'}>
+              <em>0{index + 1}</em>
+              <div className={'popularItem-info'}>
+                <Link href={'/posts/[id]'} as={`/posts/${post.id}`}>
+                  <a><cite className={'title'}>{post.title}</cite></a>
+                </Link>
+                <p className={'author'}>作者</p>
+                <time className={'time'}>{new Date(post.createdAt).toDateString()}</time>
+              </div>
             </div>
-          </div>
+          ))}
         </PopularList>
       </PageMain>
-      {posts.map(post => <p key={post.id}><Link href={'/posts/[id]'}
-                                                as={`/posts/${post.id}`}><a>{post.id}---{post.title}({post.authorId})</a></Link>
-      </p>)}
       {pager}
     </Container>
   );
---
+++ pages/posts/indexStyles.tsx
@@ -0,0 +1,198 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 1200px;
+  min-width: 900px;
+  margin: 0 auto;
+  padding: 0 10px;
+  .title {
+    display: inline-block;
+    font-weight: 600;
+    font-style: normal;
+  }
+  .author {
+    cursor: pointer;
+    color: rgba(0, 0, 0, 0.84);
+    font-size: 15px;
+    margin: 8px 0;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+  .content {
+    font-weight: 400;
+    color: rgba(0, 0, 0, 0.54);
+    font-size: 16px;
+    margin-bottom: 20px;
+    line-height: 1.4;
+  }
+  .time {
+    color: rgba(0, 0, 0, 0.54);
+    font-size: 15px;
+  }
+`
+export const PageHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0;
+`
+export const ImageWrapper = styled.div`
+  width: 150px;
+`
+export const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  > a {
+    background-color: rgb(212,80,80);
+    color: #fff;
+    padding: .6em 1.1em;
+    border-radius: 2em;
+    text-decoration: none;
+    transition: all .3s;
+    &:hover {
+      background-color: rgb(212,60,60);
+    }
+  }
+  > * {
+    margin: 0 20px;
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+`
+export const UserHeaderWrapper = styled.div`
+  border-radius: 50%;
+  width: 35px;
+  height: 35px;
+  overflow: hidden;
+  border: 1px solid #ddd;
+`
+export const CategoryWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 10px 0 20px 0;
+`
+export const Category = styled.ul`
+  list-style: none;
+  flex: 1;
+  display: inline-flex;
+  .item {
+    margin: 0 10px;
+    color: rgba(0,0,0,.54);
+    transition: all .3s;
+    &.active {
+      color: rgba(0,0,0,.84);
+    }
+  }
+`
+export const LatestPostsWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+export const LeftSection = styled.section`
+  width: 48%;
+  .imageWrapper {
+    background-color: #ddd;
+    height: 150px;
+    overflow: hidden;
+  }
+  .title {
+    font-size: 24px;
+    line-height: 28px;
+    margin: 17px 0 13px 0;
+  }
+`
+export const RightSection = styled.section`
+  width: 49%;
+  .articleItem {
+    display: flex;
+    margin-bottom: 20px;
+    .imageWrapper {
+      width: 100px;
+      height: 100px;
+      overflow: hidden;
+      margin-right: 20px;
+      background-color: #ddd;
+    }
+    .postInfo {
+      flex: 1;
+      display: inline-flex;
+      flex-direction: column;
+      overflow: hidden;
+      .title {
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        font-size: 18px;
+        line-height: 20px;
+        margin-bottom: auto;
+        display: block;
+      }
+      .content {
+        margin-top: 5px;
+        margin-bottom: 8px;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+      }
+    }
+  }
+`
+export const PageMain = styled.main`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 60px;
+  margin-top: 40px;
+  border-top: 1px solid #ddd;
+`
+export const ArticleList = styled.ul`
+  list-style: none;
+  flex: 1;
+  .articleItem {
+    display: flex;
+    margin-bottom: 20px;
+    &-info {
+      flex: 1;
+      .title {
+        font-size: 24px;
+        line-height: 28px;
+        margin-bottom: 12px;
+      }
+    }
+    .imageWrapper {
+      width: 150px;
+      margin-left: 25px;
+      height: 150px;
+      overflow: hidden;
+      background-color: #ddd;
+    }
+  }
+`
+export const PopularList = styled.div`
+  width: 320px;
+  margin-left: 50px;
+  h4 {
+    font-weight: 600;
+    color: rgba(0,0,0,.84);
+    font-size: 24px;
+    padding-bottom: 20px;
+    border-bottom: 1px solid #ddd;
+  }
+  .popularItem {
+    display: flex;
+    padding-top: 36px;
+    em {
+      font-size: 34px;
+      color: rgba(0,0,0,.15);
+      font-style: normal;
+    }
+    &-info {
+      margin-left: 20px;
+      .title {
+        font-size: 18px;
+        margin-bottom: 10px;
+      }
+    }
+  }
+`
