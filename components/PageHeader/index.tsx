eae48a383113a862e6efe0180bc75cc3b1dd552f
zhangchao <529743595@qq.com>
2021-05-17 22:23:42 +0800

    feat: add search ui

--- components/PageHeader/index.tsx
+++ components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import headerImage from '../../assets/header.png';
 import ALiIcon from '../ALiIcon';
 import Link from 'next/link';
@@ -28,8 +28,9 @@ const UserInfo = styled.div`
       background-color: rgb(212,60,60);
     }
   }
-  > svg {
+  .searchIcon, .messageIcon {
     color: #999;
+    cursor: pointer;
   }
   > * {
     margin: 0 14px;
@@ -50,10 +51,33 @@ const UserHeaderWrapper = styled.div`
   overflow: hidden;
   border: 1px solid #ddd;
 `
+const SearchBarWrapper = styled.div`
+  overflow: hidden;
+`
+const SearchBar = styled.div`
+  display: flex;
+  align-items: center;
+  transition: all .25s;
+  &.hidden {
+    transform: translateX(156px);
+  }
+  input {
+    border: none;
+    outline: none;
+    margin-left: 6px;
+    width: 150px;
+    font-size: 13px;
+  }
+`
 type Props = {
   user: User;
 }
 const PageHeader: React.FC<Props> = ({user}) => {
+  const [searchBarHidden, setSearchBarHidden] = useState(true)
+  const [searchWord, setSearchWord] = useState('')
+  const search = useCallback(() => {
+    console.log(searchWord);
+  }, [searchWord])
   return (
     <Header>
       <Link href="/posts"><a>
@@ -62,8 +86,13 @@ const PageHeader: React.FC<Props> = ({user}) => {
         </ImageWrapper>
       </a></Link>
       <UserInfo>
-        <ALiIcon icon={'search'} />
-        <ALiIcon icon={'message'} />
+        <SearchBarWrapper>
+          <SearchBar className={searchBarHidden ? 'hidden' : ''}>
+            <ALiIcon onClick={() => setSearchBarHidden(!searchBarHidden)} className={'searchIcon'} icon={'search'} />
+            <input value={searchWord} onChange={e => setSearchWord(e.target.value)} onKeyDown={e => e.keyCode === 13 && search()} placeholder={'请搜索文章'} type="text"/>
+          </SearchBar>
+        </SearchBarWrapper>
+        <ALiIcon className={'messageIcon'} icon={'message'} />
         {user ? <Link href="/user/[id]" as={`/user/${user.id}`}><a className={'user'}>
           <UserHeaderWrapper>
             <img width="130%" src={ logoImage } alt=""/>
