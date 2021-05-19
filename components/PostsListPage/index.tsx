a26425e30f9073676a9a3b723d48314e872d56ff
zhangchao <529743595@qq.com>
2021-05-19 13:30:07 +0800

    styles: highlight title

--- components/PostsListPage/index.tsx
+++ components/PostsListPage/index.tsx
@@ -4,6 +4,7 @@ import PageHeader from '../PageHeader';
 import Link from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
 const Container = styled.div`
   max-width: 1200px;
@@ -20,7 +21,10 @@ const Container = styled.div`
     border-bottom: 1px solid #ddd;
     padding: .7em 0;
     font-size: 18px;
-    .content {
+    &-title {
+      em {color: #ea4335;font-style: normal;}
+    }
+    &-content {
       color: #999;
       font-size: 14px;
       margin-top: .4em;
@@ -39,13 +43,19 @@ interface Props {
 }
 
 const PostsListPage: NextPage<Props> = ({ pageTitle, user, posts, ...pageOption }) => {
+  const router = useRouter()
   const { pager } = userPager(pageOption);
   return (
     <Container>
       <PageHeader user={user} />
-      <h1 className={'authorTitle'}>{pageTitle} 的博客</h1>
+      <h1 className={'authorTitle'}>"{pageTitle}" 的博客</h1>
       <section className={'posts'}>
-        {posts.map(post => <Link href="/posts/[id]" key={post.id} as={`/posts/${post.id}`}><a><div className={'postItem'}><p className={'postItem-title'}>{post.title}</p><p className={'content'}>{post.content.slice(0, 100)}...</p></div></a></Link>)}
+        {posts.map(post => <Link href="/posts/[id]" key={post.id} as={`/posts/${post.id}`}><a>
+          <div className={'postItem'}>
+            <p className={'postItem-title'} dangerouslySetInnerHTML={{__html: post.title.replace(router.query.word?.toString(), `<em>${router.query.word}</em>`)}} />
+            <p className={'postItem-content'}>{post.content.slice(0, 100)}...</p>
+          </div>
+        </a></Link>)}
       </section>
       { pager }
     </Container>
