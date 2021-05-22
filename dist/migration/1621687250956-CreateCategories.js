33d558c4226e43efdf0a744796a0d2ac23f18f9a
zhangchao <529743595@qq.com>
2021-05-22 21:03:43 +0800

    feat: add create category migration

---
+++ dist/migration/1621687250956-CreateCategories.js
@@ -0,0 +1,103 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.CreateCategories1621687250956 = void 0;
+
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
+
+var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
+
+var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
+
+var _typeorm = require("typeorm");
+
+var CreateCategories1621687250956 = /*#__PURE__*/function () {
+  function CreateCategories1621687250956() {
+    (0, _classCallCheck2["default"])(this, CreateCategories1621687250956);
+  }
+
+  (0, _createClass2["default"])(CreateCategories1621687250956, [{
+    key: "up",
+    value: function () {
+      var _up = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(queryRunner) {
+        return _regenerator["default"].wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                _context.next = 2;
+                return queryRunner.createTable(new _typeorm.Table({
+                  name: 'categories',
+                  columns: [{
+                    name: 'id',
+                    isGenerated: true,
+                    type: 'int',
+                    isPrimary: true,
+                    generationStrategy: 'increment'
+                  }, {
+                    name: 'name',
+                    type: 'varchar'
+                  }]
+                }));
+
+              case 2:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee);
+      }));
+
+      function up(_x) {
+        return _up.apply(this, arguments);
+      }
+
+      return up;
+    }()
+  }, {
+    key: "down",
+    value: function () {
+      var _down = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee2(queryRunner) {
+        return _regenerator["default"].wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                _context2.next = 2;
+                return queryRunner.dropTable(new _typeorm.Table({
+                  name: 'categories',
+                  columns: [{
+                    name: 'id',
+                    isGenerated: true,
+                    type: 'int',
+                    isPrimary: true,
+                    generationStrategy: 'increment'
+                  }, {
+                    name: 'name',
+                    type: 'varchar'
+                  }]
+                }));
+
+              case 2:
+              case "end":
+                return _context2.stop();
+            }
+          }
+        }, _callee2);
+      }));
+
+      function down(_x2) {
+        return _down.apply(this, arguments);
+      }
+
+      return down;
+    }()
+  }]);
+  return CreateCategories1621687250956;
+}();
+
+exports.CreateCategories1621687250956 = CreateCategories1621687250956;
---
+++ src/migration/1621687250956-CreateCategories.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCategories1621687250956 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'categories',
+            columns: [
+                {name: 'id', isGenerated: true, type: 'int', isPrimary: true, generationStrategy: 'increment'},
+                {name: 'name', type: 'varchar'},
+            ],
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable(new Table({
+            name: 'categories',
+            columns: [
+                {name: 'id', isGenerated: true, type: 'int', isPrimary: true, generationStrategy: 'increment'},
+                {name: 'name', type: 'varchar'},
+            ],
+        }));
+    }
+
+}
